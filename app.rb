c0e9ebbe2da8b9d3c3a34516334404644a62a517
KEVIN KELLER <keller_kevin@bah.com>
2017-02-14 15:29:05 -0500

    Initial

---
+++ app.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+require 'pg'
+require_relative 'db/connection'
+require_relative 'models/pokemon'
+
+
+get '/pokemon' do
+  @pokemon = Pokemon.all
+  erb :"pokemon/index"
+end
+
+get '/pokemon/new' do
+  erb :"pokemon/new"
+end
+
+get '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemon/show"
+end
+
+get '/pokemon/edit' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemon/edit"
+end
+
+post '/pokemon/:id' do
+  @pokemon = Pokemon.create(params[:pokemon])
+  redirect("/pokemon/#{@pokemon.id}")
+end
+
+delete '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.destroy
+  redirect ("/pokemon")
+end
